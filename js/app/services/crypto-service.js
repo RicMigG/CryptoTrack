3bdc0dbd4cb2b2629270eca3e5628411d7ff4701
Ricardo Gago <ricardomgago@gmail.com>
2023-04-13 22:15:43 +0100

    Page now populates with data

--- js/app/services/crypto-service.js
+++ js/app/services/crypto-service.js
@@ -1,34 +1,56 @@
-define(function() {
-    var internals = {}; // internal state
-    var externals = {}; // external api
+define(function () {
+  var internals = {}; // internal state
+  var externals = {}; // external api
 
-    internals.getAllInformation = () => {
-        $.ajax({
-        url: 'https://api.coincap.io/v2/assets',
-        beforeSend: function(xhr) {
-            xhr.setRequestHeader('Authorization', 'Bearer ' + '73c931ec-a0d2-4a4f-8cc8-030689b7d007');
-          },
-        async: true,
-        dataType: "json",
-            success: function(results) { internals.handleAllData(null, results) },
-            error: function(request, statusText, httpError) { handleAllData(httpError || statusText) }
-        })};
+  internals.getAllInformation = () => {
+    fetch("https://api.coincap.io/v2/assets", {
+      headers: {
+        "Accept-Encoding": "gzip, deflate",
+        Authorization: "Bearer 73c931ec-a0d2-4a4f-8cc8-030689b7d007",
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        internals.populateWebsite(response);
+      })
+      .catch((error) => {
+        throw Error("Failure with AJAX request: " + error);
+      });
+  };
 
-    internals.handleAllData = function(error, results) {
-        if(error) {
-            throw Error("Failure with AJAX request.");
-        }
-        internals.data = results;
-    };
+  internals.populateWebsite = (allCoinsData) => {
+    let row;
+    for (let index = 1; index < 25; index++) {
+      const element = allCoinsData.data[index - 1];
+      let appendToTarget = `#row-0`;
 
-
-    externals.startAjax = () => {
-        internals.getAllInformation();
+      $(`<div class="col-sm-3">
+            <div id="${element.id}" class="card border-0">
+                <img class="card-img-top p-5" src="https://assets.coincap.io/assets/icons/${element.symbol.toLowerCase()}@2x.png" alt="Card image cap" />
+                <div class="card-body text-center">
+                    <h5 class="card-title">${element.name}</h5>
+                    <p><strong>Price: </strong> $${Number(
+                      element.priceUsd
+                    ).toFixed(2)}</p>
+                    <p><strong>Last 24h: </strong> ${Number(
+                      element.changePercent24Hr
+                    ).toFixed(2)}%</p>
+                </div>
+                
+            </div>
+        </div>`).appendTo(appendToTarget);
     }
+  };
 
-    externals.getCoins = function(cb) {
-        cb(internals.data);
-    };
+  externals.startAjax = () => {
+    internals.getAllInformation();
+  };
 
-    return externals;
+  externals.getCoins = function (cb) {
+    cb(internals.data);
+  };
+
+  return externals;
 });

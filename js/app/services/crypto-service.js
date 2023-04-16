33e8a685ef324a00cdc8a4a9de5315c44cb8413c
Ricardo Gago <ricardomgago@gmail.com>
2023-04-16 20:11:39 +0100

    Improved the MVC configuration of the APP

--- js/app/services/crypto-service.js
+++ js/app/services/crypto-service.js
@@ -1,4 +1,4 @@
-define(function () {
+define(["views/crypto-view"], function (cryptoView) {
   var internals = {}; // internal state
   var externals = {}; // external api
 
@@ -13,43 +13,19 @@ define(function () {
         return response.json();
       })
       .then((response) => {
-        internals.populateWebsite(response);
+        internals.passInformationToView(response);
       })
       .catch((error) => {
         throw Error("Failure with AJAX request: " + error);
       });
   };
 
-  internals.populateWebsite = (allCoinsData) => {
-    let row;
-    for (let index = 1; index < 25; index++) {
-      const element = allCoinsData.data[index - 1];
-      let appendToTarget = `#row-0`;
-
-      $(`<div class="col-sm-3">
-            <div id="${element.id}" class="card border-0">
-                <img class="card-img-top p-5" src="https://assets.coincap.io/assets/icons/${element.symbol.toLowerCase()}@2x.png" alt="Card image cap" />
-                <div class="card-body text-center">
-                    <h5 class="card-title">${element.name}</h5>
-                    <p><strong>Price: </strong> $${Number(
-                      element.priceUsd
-                    ).toFixed(2)}</p>
-                    <p><strong>Last 24h: </strong> ${Number(
-                      element.changePercent24Hr
-                    ).toFixed(2)}%</p>
-                </div>
-                
-            </div>
-        </div>`).appendTo(appendToTarget);
-    }
+  internals.passInformationToView = (cryptoData) => {
+    cryptoView.insertDataIntoWebsite(cryptoData);
   };
 
   externals.startAjax = () => {
     internals.getAllInformation();
-  };
-
-  externals.getCoins = function (cb) {
-    cb(internals.data);
   };
 
   return externals;
--- js/app/views/crypto-view.js
+++ js/app/views/crypto-view.js
@@ -1,12 +1,56 @@
-define(function() {
-    var internals = {
-        handlers: {},
-        elements: {}
-    };
+define(function () {
+  let internals = {};
 
-    var externals = {};
+  let externals = {};
 
-    
+  internals.populateWebsite = (allCoinsData) => {
+    for (let index = 1; index < 25; index++) {
+      const element = allCoinsData.data[index - 1];
+      let appendToTarget = `#row-0`;
 
-    return externals;
+      $(`<div class="col-sm-3">
+                <div id="${element.id}" class="card border-0 cardClick">
+                    <img class="card-img-top p-5" src="https://assets.coincap.io/assets/icons/${element.symbol.toLowerCase()}@2x.png" alt="Card image cap" />
+                    <div class="card-body text-center">
+                        <h5 class="card-title">${element.name}</h5>
+                        <p><strong>Price: </strong> $${Number(
+                          element.priceUsd
+                        ).toFixed(2)}</p>
+                        <p><strong>Last 24h: </strong> ${Number(
+                          element.changePercent24Hr
+                        ).toFixed(2)}%</p>
+                    </div>
+                </div>
+            </div>`).appendTo(appendToTarget);
+    }
+    internals.attachEventHandlers();
+  };
+
+  internals.attachEventHandlers = () => {
+    $(".cardClick").click((event) => {
+      event.preventDefault();
+      let clickedElement = event.target;
+      while (clickedElement && !clickedElement.id) {
+        clickedElement = clickedElement.parentElement;
+      }
+      if (clickedElement) {
+        let parentId = clickedElement.id;
+        alert(parentId);
+      }
+    });
+  };
+
+  internals.clearPanel = () => {
+    $(".row").empty();
+  };
+
+  externals.insertDataIntoWebsite = (coinsData) => {
+    internals.populateWebsite(coinsData);
+  };
+
+  externals.prepareViewForNewLoading = () => {
+    internals.clearPanel();
+  };
+
+  return externals;
 });
